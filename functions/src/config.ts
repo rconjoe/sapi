ea62ea6d73043c5546c03c31d2c02f2ec11ee945
trogdor the burninator <joe@rconjoe.com>
2020-09-11 01:53:32 -0400

    include api versioning on stripe instantiation
    this was an issue causing stripe to instantiate improperly. you need to pass both the api key and the version.

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -1,17 +1,17 @@
-// Initialize Firebase Admin
+// initialize firebase admin
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp();
 
-// Initialize Cloud Firestore Database
+// initialize firestore db
 export const db = admin.firestore();
 const settings = { timestampsInSnapshots: true };
 db.settings(settings);
 
-// ENV Variables
+// env variables
 export const stripeSecret = functions.config().stripe.secret;
 
-// Export Stripe
+// instantiate and export stripe - include api versioning
 import Stripe from 'stripe'; 
 export const stripe = new Stripe(stripeSecret, {
     apiVersion: '2020-08-27',
